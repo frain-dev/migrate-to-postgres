fb01934c679741ce4875ff5ee892b4e5045fd7fc
Daniel Oluojomu <danvixent@gmail.com>
2023-03-19 14:03:36 +0000

    fix: ignore endpoints that dont have project id

--- cmd/migrate_endpoints.go
+++ cmd/migrate_endpoints.go
@@ -6,6 +6,8 @@ import (
 	"fmt"
 	"math"
 
+	"github.com/frain-dev/migrate-to-postgres/convoy082/pkg/log"
+
 	"github.com/oklog/ulid/v2"
 
 	"go.mongodb.org/mongo-driver/bson"
@@ -57,7 +59,8 @@ func migrateEndpointsCollection(store datastore082.Store, dbx *sqlx.DB) error {
 
 			projectID, ok := oldIDToNewID[endpoint.ProjectID]
 			if !ok {
-				return fmt.Errorf("new project id for project %s not found for endpoint %s", endpoint.ProjectID, endpoint.UID)
+				log.Errorf("new project id for project %s not found for endpoint %s", endpoint.ProjectID, endpoint.UID)
+				continue
 			}
 
 			postgresEndpoint := &datastore09.Endpoint{
