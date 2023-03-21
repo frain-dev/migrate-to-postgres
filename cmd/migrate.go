96e4bfa3d438fabd903b537662b55738139ef159
Daniel Oluojomu <danvixent@gmail.com>
2023-03-21 01:26:55 +0000

    fix: reduce batch size to 50k

--- cmd/migrate.go
+++ cmd/migrate.go
@@ -44,7 +44,7 @@ type PG struct {
 	db *sqlx.DB
 }
 
-const batchSize int64 = 100000
+const batchSize int64 = 50000
 
 func migrate(mongoDsn, postgresDsn string) error {
 	vv := config.Configuration{
